1f93f9c54b1817d4cc89ff844b14139150e310f3
KwonDae <green201402317@gmail.com>
2024-01-04 23:36:29 +0900

    Coil3 Application 설정 주석 추가

--- composeApp/src/androidMain/kotlin/org/moneyking/imagepicker/Application.kt
+++ composeApp/src/androidMain/kotlin/org/moneyking/imagepicker/Application.kt
@@ -8,6 +8,11 @@ import org.jetbrains.compose.components.resources.BuildConfig
 import org.moneyking.imagepicker.util.newImageLoader
 
 class Application : Application(), SingletonImageLoader.Factory {
+    /**
+     * If you use the singleton ImageLoader on Android, 
+     * create a custom ImageLoader in your Application's SingletonImageLoader.Factory:
+     * https://coil-kt.github.io/coil/upgrading_to_coil3/#network-images
+     */
     override fun newImageLoader(context: PlatformContext): ImageLoader {
         return newImageLoader(this, BuildConfig.DEBUG)
     }
